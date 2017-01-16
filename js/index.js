69167f6afbc91f4c970081c9899178f7a4682be4
Yuri Thomaz de Assis Ribeiro <developer@multimidiahouse.com.br>
2017-01-16 18:55:56 -0200

    Change Console commands

--- js/index.js
+++ js/index.js
@@ -3,68 +3,193 @@ var index = {
 	jogo: new String(),
 	quantidade: new Number(),
 	html: new String(),
+	//publisherId: new String("ca-app-pub-5075057333402288/2294971952"),
+	//interstitialAdId: new String("ca-app-pub-5075057333402288/7922703150"),
+	autoShowInterstitial: false,
+	progressDialog: document.getElementById("progressDialog"),
+	spinner: document.getElementById("spinner"),
+	weinre: {
+		enabled: false,
+		ip: '', // ex. http://192.168.1.13
+		port: '', // ex. 9090
+		targetApp: '' // ex. see weinre docs
+	},
 	initialize: function(){
-		document.addEventListener('deviceready', index.onDeviceReady, false);
-	},
-	onDeviceReady: function() {
-		index.initAd();
-		index.showBannerFunc();
-		index.showInterstitialFunc();
-	},
-	initAd: function(){
-		if ( window.plugins && window.plugins.AdMob ) 
-		{
-			var ad_units = {
-				ios : {
-					banner: 'ca-app-pub-5075057333402288/2294971952',		//PUT ADMOB ADCODE HERE 
-					interstitial: 'ca-app-pub-5075057333402288/7922703150'	//PUT ADMOB ADCODE HERE 
-				},
-				android : {
-					banner: 'ca-app-pub-5075057333402288/2294971952',		//PUT ADMOB ADCODE HERE 
-					interstitial: 'ca-app-pub-5075057333402288/7922703150'	//PUT ADMOB ADCODE HERE 
-				}
-			};
-			var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
- 
-			window.plugins.AdMob.setOptions( {
-				publisherId: admobid.banner,
-				interstitialAdId: admobid.interstitial,
-				adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
-				bannerAtTop: true, // set to true, to put banner at top 
-				overlap: true, // banner will overlap webview  
-				offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
-				isTesting: true, // receiving test ad 
-				autoShow: true // auto show interstitial ad when loaded 
-			});
- 
-			index.registerAdEvents();
-			window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown 
-			window.plugins.AdMob.requestInterstitialAd();
- 
+		if ((/(ipad|iphone|ipod|android)/i.test(navigator.userAgent))) {
+			document.addEventListener('deviceready', index.onDeviceReady, false);
 		} else {
-			alert( 'admob plugin not ready' ); 
+			index.onDeviceReady();
 		}
 	},
-	registerAdEvents: function() {
-			document.addEventListener('onReceiveAd', function(){});
-			document.addEventListener('onFailedToReceiveAd', function(data){});
-			document.addEventListener('onPresentAd', function(){});
-			document.addEventListener('onDismissAd', function(){ });
-			document.addEventListener('onLeaveToAd', function(){ });
-			document.addEventListener('onReceiveInterstitialAd', function(){ });
-			document.addEventListener('onPresentInterstitialAd', function(){ });
-			document.addEventListener('onDismissInterstitialAd', function(){
-				window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW 
-				window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed 
+	initAds: function () {
+		var isAndroid = (/(android)/i.test(navigator.userAgent));
+		var adPublisherIds = {
+			ios: {
+				banner: 'ca-app-pub-9863325511078756/5232547029',
+				interstitial: 'ca-app-pub-9863325511078756/6709280228'
+			},
+			android: {
+				banner: 'ca-app-pub-9863325511078756/9802347428',
+				interstitial: 'ca-app-pub-9863325511078756/2279080628'
+			}
+		};
+		var admobid;
+
+		if (isAndroid) {
+			admobid = adPublisherIds.android;
+		} else {
+			admobid = adPublisherIds.ios;
+		}
+		if (window.admob) {
+			admob.setOptions({
+				publisherId: admobid.banner,
+				interstitialAdId: admobid.interstitial,
+				bannerAtTop: true, // set to true, to put banner at top
+				overlap: false, // set to true, to allow banner overlap webview
+				offsetStatusBar: true, // set to true to avoid ios7 status bar overlap
+				isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
+				autoShowBanner: true, // auto show banners ad when loaded
+				autoShowInterstitial: false // auto show interstitials ad when loaded
 			});
+		} else {
+			alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+		}
 	},
-	showBannerFunc: function(){
-		window.plugins.AdMob.createBannerView();
+	bindAdEvents: function () {
+		if (window.admob) {
+			document.addEventListener("orientationchange", this.onOrientationChange, false);
+			document.addEventListener(admob.events.onAdLoaded, this.onAdLoaded, false);
+			document.addEventListener(admob.events.onAdFailedToLoad, this.onAdFailedToLoad, false);
+			document.addEventListener(admob.events.onAdOpened, function (e) { }, false);
+			document.addEventListener(admob.events.onAdClosed, function (e) { }, false);
+			document.addEventListener(admob.events.onAdLeftApplication, function (e) { }, false);
+			document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) { }, false);
+		} else {
+			alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+		}
 	},
-	showInterstitialFunc: function(){
-		window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown and show when it's loaded. 
-		window.plugins.AdMob.requestInterstitialAd();
+    // -----------------------------------
+    // Events.
+    // The scope of 'this' is the event.
+    // -----------------------------------
+    onOrientationChange: function () {
+        index.onResize();
+    },
+	onDeviceReady: function() {
+		var weinre, weinreUrl;
+
+		document.removeEventListener('deviceready', index.onDeviceReady, false);
+
+		if (index.weinre.enabled) {
+			//console.log('Loading weinre...');
+			alert('Loading weinre');
+			weinre = document.createElement('script');
+			weinreUrl = index.weinre.ip + ":" + index.weinre.port;
+			weinreUrl += '/target/target-script-min.js';
+			weinreUrl += '#' + index.weinre.targetApp;
+			weinre.setAttribute('src', weinreUrl);
+			document.head.appendChild(weinre);
+		}
+
+		if (window.admob) {
+			//console.log('Binding ad events...');
+			alert('Binding ad events...');
+			index.bindAdEvents();
+			//console.log('Initializing ads...');
+			alert('Initializing ads...');
+			index.initAds();
+		} else {
+			alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+		}
 	},
+    onAdLoaded: function (e) {
+        index.showProgress(false);
+        if (window.admob && e.adType === window.admob.AD_TYPE.INTERSTITIAL) {
+            if (index.autoShowInterstitial) {
+                window.admob.showInterstitialAd();
+            } else {
+                alert("Interstitial is available. Click on 'Show Interstitial' to show it.");
+            }
+        }
+    },
+    onAdFailedToLoad: function (e) {
+        index.showProgress(false);
+        alert("Could not load ad: " + JSON.stringify(e));
+    },
+    onResize: function () {
+        var msg = 'Web view size: ' + window.innerWidth + ' x ' + window.innerHeight;
+        document.getElementById('sizeinfo').innerHTML = msg;
+    },
+
+    // -----------------------------------
+    // App buttons functionality
+    // -----------------------------------
+    startBannerAds: function () {
+        if (window.admob) {
+            index.showProgress(true);
+            window.admob.createBannerView(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    removeBannerAds: function () {
+        if (window.admob) {
+            index.showProgress(false);
+            window.admob.destroyBannerView();
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showBannerAds: function () {
+        if (window.admob) {
+            index.showProgress(false);
+            window.admob.showBannerAd(true, function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    hideBannerAds: function () {
+        if (window.admob) {
+            index.showProgress(false);
+            window.admob.showBannerAd(false);
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    requestInterstitial: function (autoshow) {
+        if (window.admob) {
+            index.showProgress(true);
+            index.autoShowInterstitial = autoshow;
+            window.admob.requestInterstitialAd(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showInterstitial: function () {
+        if (window.admob) {
+            index.showProgress(false);
+            window.admob.showInterstitialAd(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showProgress: function (show) {
+        if (show) {
+            addClass(index.spinner, "animated");
+            removeClass(index.progressDialog, "hidden");
+        } else {
+            addClass(index.progressDialog, "hidden");
+            removeClass(index.spinner, "animated");
+        }
+    },
 	sorteio: function(jogo){
 		marcacao = 0;
 		numeros = 0;
@@ -159,4 +284,16 @@ var index = {
 			$(obj).html('Ver Números');
 		}
 	}	
+};
+
+function removeClass(elem, cls) {
+    var str;
+    do {
+        str = " " + elem.className + " ";
+        elem.className = str.replace(" " + cls + " ", " ").replace(/^\s+|\s+$/g, "");
+    } while (str.match(cls));
+}
+
+function addClass(elem, cls) {
+    elem.className += (" " + cls);
 }
