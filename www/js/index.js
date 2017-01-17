daa2e9a733294f7a53f71f00f2b86c68fd7f5cf6
Yuri Thomaz de Assis Ribeiro <developer@multimidiahouse.com.br>
2017-01-16 22:49:48 -0200

    Change Ads to Base

--- www/js/index.js
+++ www/js/index.js
@@ -42,7 +42,7 @@ var app = {
             admob.setOptions({
                 publisherId: admobid.banner,
                 interstitialAdId: admobid.interstitial,
-                bannerAtTop: true, // set to true, to put banner at top
+                bannerAtTop: false, // set to true, to put banner at top
                 overlap: false, // set to true, to allow banner overlap webview
                 offsetStatusBar: true, // set to true to avoid ios7 status bar overlap
                 isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
